485fba138e5af68589a25be5621544eaca158299
thiesen <nathan.thiesen@outlook.com>
2015-06-22 16:14:14 -0300

    create main social networks test cases for parser

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -1,6 +1,6 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'traffic_source_parser'
 
-require 'minitest/unit'
 require 'minitest/autorun'
+require 'minitest/unit'
 require 'minitest/pride'
--- test/test_traffic_source_parser_test.rb
+++ test/test_traffic_source_parser_test.rb
@@ -5,7 +5,13 @@ class TestTrafficSourceParser < Minitest::Test
     refute_nil ::TrafficSourceParser::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_that_parser_returns_the_correct_social_media_source
+    assert_equal "Facebook", TrafficSourceParser.parse("https://www.facebook.com/")
+    assert_equal "Twitter", TrafficSourceParser.parse("http://t.co/W1pX6dNa2V")
+    assert_equal "LinkedIn", TrafficSourceParser.parse("https://www.linkedin.com/")
+    assert_equal "Google Plus", TrafficSourceParser.parse("http://plus.url.google.com/url?sa=j&url=http%3A%2F%2Fbit.ly%2F4thofJulyPartyIdeas&uct=1434564324&usg=EGzOjzYKd_AM9NR0icjtoT1p_54")
+    assert_equal "Pinterest", TrafficSourceParser.parse("https://www.pinterest.com/")
   end
+
+
 end
