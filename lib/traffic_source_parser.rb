b3a2723952aa4b9d67e2563d1751e6c97a687ac5
thiesen <nathan.thiesen@outlook.com>
2015-06-22 16:48:29 -0300

    working parser for social

--- lib/traffic_source_parser.rb
+++ lib/traffic_source_parser.rb
@@ -1,5 +1,29 @@
 require "traffic_source_parser/version"
+require "traffic_source_parser/social"
+require "traffic_source_parser/domain_tools"
+require 'public_suffix'
+require 'yaml'
 
 module TrafficSourceParser
-  # Your code goes here...
+  extend self
+
+  extend DomainTools
+
+  def parse(referrer)
+    @referrer = referrer
+    source_parser
+  end
+
+  def source_parser
+    TrafficSourceParser.sources_list.each do |source_type, sources|
+      _, src = sources.detect {|key, _| key =~ referrer_regex } #find
+      parser = self.const_get source_type.capitalize
+      return parser.process(@referrer, src)
+    end
+  end
+
+  def sources_list
+    @@sources_lists ||= YAML.load_file './config/sources.yml'
+  end
+
 end
--- test/test_traffic_source_parser_test.rb
+++ test/test_traffic_source_parser_test.rb
@@ -9,7 +9,11 @@ class TestTrafficSourceParser < Minitest::Test
     assert_equal "Facebook", TrafficSourceParser.parse("https://www.facebook.com/")
     assert_equal "Twitter", TrafficSourceParser.parse("http://t.co/W1pX6dNa2V")
     assert_equal "LinkedIn", TrafficSourceParser.parse("https://www.linkedin.com/")
-    assert_equal "Google Plus", TrafficSourceParser.parse("http://plus.url.google.com/url?sa=j&url=http%3A%2F%2Fbit.ly%2F4thofJulyPartyIdeas&uct=1434564324&usg=EGzOjzYKd_AM9NR0icjtoT1p_54")
+    assert_equal "Google Plus", TrafficSourceParser.parse("http://plus.url.google.com/url" +
+                                                          "?sa=j&url=http%3A%2F%2Fbit.ly" +
+                                                          "%2F4thofJulyPartyIdeas&uct=" +
+                                                          "1434564324&usg=" +
+                                                          "EGzOjzYKd_AM9NR0icjtoT1p_54")
     assert_equal "Pinterest", TrafficSourceParser.parse("https://www.pinterest.com/")
   end
 
