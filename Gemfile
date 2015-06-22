6df63da9dc57b9059b9c2d3bfe2da740ec383150
thiesen <nathan.thiesen@outlook.com>
2015-06-22 16:47:29 -0300

    almost working parser

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
 
-# Specify your gem's dependencies in traffic_source_parser.gemspec
 gemspec
+
+gem 'public_suffix'
---
+++ lib/traffic_source_parser/social.rb
@@ -0,0 +1,12 @@
+module TrafficSourceParser
+  module Social
+    extend self
+
+    def process(referrer, source)
+      return source
+    end
+
+  end
+
+end
+
