dad0c6e52d9582851e1636ed9e3df91a92996bbf
thiesen <nathan.thiesen@outlook.com>
2015-06-24 15:06:30 -0300

    put code climate test reporter at first line of spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,10 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-
-require 'traffic_source_parser'
-require 'pry'
 require "codeclimate-test-reporter"
 
 CodeClimate::TestReporter.start
+
+require 'traffic_source_parser'
+require 'pry'
+require 'rspec'
+
+
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
