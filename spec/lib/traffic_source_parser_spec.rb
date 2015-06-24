a25ab7d499821edb6e7eb50e8ecacc8d39d7917d
thiesen <nathan.thiesen@outlook.com>
2015-06-24 13:07:07 -0300

    create campaign parser spec

--- spec/lib/traffic_source_parser_spec.rb
+++ spec/lib/traffic_source_parser_spec.rb
@@ -2,6 +2,19 @@ require 'spec_helper'
 
 describe TrafficSourceParser do
 
+  shared_examples_for "returns the correct object with attributes" do
+    it "returns klass object with hash attributes" do
+       sources.each do |source|
+          cookie_value = source.delete(:cookie_value)
+          parsed_result = TrafficSourceParser.parse(cookie_value)
+          expect(parsed_result).to be_a klass
+          source.keys.each do |key|
+            expect(parsed_result.send(key)).to eq source[key]
+          end
+        end
+    end
+  end
+
   it 'has a version number' do
     expect(TrafficSourceParser::VERSION).not_to be nil
   end
@@ -32,48 +45,49 @@ describe TrafficSourceParser do
 
     context "when source is utmz" do
 
-      let(:utmz_sources) do
+      let(:sources) do
         [
           {
-            utmz: "utmz:256172697.1432831709.1.1.utmcsr=(direct)|utmccn=(dir" +
-                  "ect)|utmcmd=(none)",
+            cookie_value: "utmz:256172697.1432831709.1.1.utmcsr=(direct)|utm" +
+                          "ccn=(direct)|utmcmd=(none)",
             utmcsr: "(direct)",
             utmccn: "(direct)",
             utmcmd: "(none)"
           },
           {
-            utmz: "utmz:231152653.1432828491.1.1.utmcsr=t.co|utmccn=(referra" +
-                  "l)|utmcmd=referral|utmcct=/EFzCFawFrk",
+            cookie_value: "utmz:231152653.1432828491.1.1.utmcsr=t.co|utmccn" +
+                          "=(referral)|utmcmd=referral|utmcct=/EFzCFawFrk",
             utmcsr: "t.co",
             utmccn: "(referral)",
             utmcmd: "referral",
             utmcct: "/EFzCFawFrk"
           },
           {
-            utmz: "utmz:10083233.1432828099.1.1.utmcsr=facebook.com|utmccn=2" +
-                  "0150528-ef-aprovacaoharvard|utmcmd=socialmedia-fe",
+            cookie_value: "utmz:10083233.1432828099.1.1.utmcsr=facebook.com|" +
+                          "utmccn=20150528-ef-aprovacaoharvard|utmcmd=social" +
+                          "media-fe",
             utmcsr: "facebook.com",
             utmccn: "20150528-ef-aprovacaoharvard",
             utmcmd: "socialmedia-fe"
           },
           {
-            utmz: "utmz:153788330.1432828657.1.1.utmcsr=(direct)|utmccn=(dir" +
-                  "ect)|utmcmd=(none)",
+            cookie_value: "utmz:153788330.1432828657.1.1.utmcsr=(direct)|utm" +
+                          "ccn=(direct)|utmcmd=(none)",
             utmcsr: "(direct)",
             utmccn: "(direct)",
             utmcmd: "(none)"
           },
           {
-            utmz: "utmz:210677130.1432831711.1.1.utmcsr=rakuten|utmccn=links" +
-                  "hare|utmcmd=(not set)",
+            cookie_value: "utmz:210677130.1432831711.1.1.utmcsr=rakuten|utmc" +
+                          "cn=linkshare|utmcmd=(not set)",
             utmcsr: "rakuten",
             utmccn: "linkshare",
             utmcmd: "(not set)"
           },
           {
-            utmz: "utmz:10083233.1432828147.6.5.utmcsr=newsletter|utmccn=201" +
-                  "50527|utmcmd=newsletter-ef|utmctr=communitycolleges|utmcc" +
-                  "t=programa",
+            cookie_value: "utmz:10083233.1432828147.6.5.utmcsr=newsletter|ut" +
+                          "mccn=20150527|utmcmd=newsletter-ef|utmctr=communi" +
+                          "tycolleges|utmcct=programa",
             utmcsr: "newsletter",
             utmccn: "20150527",
             utmcmd: "newsletter-ef",
@@ -81,9 +95,9 @@ describe TrafficSourceParser do
             utmcct: "programa"
           },
           {
-            utmz: "utmz:63514687.1432831892.1.1.utmcsr=adwords_gereboletos1m" +
-                  "in5_22072014|utmgclid=CNjQrtjy5MUCFQYXHwod7k0A8g|utmccn=(" +
-                  "not set)|utmcmd=(not set)",
+            cookie_value: "utmz:63514687.1432831892.1.1.utmcsr=adwords_gereb" +
+                          "oletos1min5_22072014|utmgclid=CNjQrtjy5MUCFQYXHwo" +
+                          "d7k0A8g|utmccn=(not set)|utmcmd=(not set)",
             utmcsr: "adwords_gereboletos1min5_22072014",
             utmgclid: "CNjQrtjy5MUCFQYXHwod7k0A8g",
             utmccn: "(not set)",
@@ -92,18 +106,57 @@ describe TrafficSourceParser do
         ]
       end
 
-      it "returns TrafficSourceParser::Parsers::UtmzParser with attributes" do
-        utmz_sources.each do |source|
-          utmz = source.delete(:utmz)
-          parsed_result = TrafficSourceParser.parse(utmz)
-          expect(parsed_result).to be_a TrafficSourceParser::Parsers::UtmzParser::Utmz
-          source.keys.each do |key|
-            expect(parsed_result.send(key)).to eq source[key]
-          end
-        end
+      let(:klass) { TrafficSourceParser::Parsers::UtmzParser::Utmz }
+
+      it_behaves_like "returns the correct object with attributes"
+
+    end
+
+    context "when source is campaign" do
+
+      let(:sources) do
+        [
+          {
+            cookie_value: "http://www.example.com/?utm_campaign=spring&utm_m" +
+                          "edium=referral&utm_source=exampleblog",
+            utm_campaign: "spring" ,
+            utm_medium: "referral" ,
+            utm_source: "exampleblog"
+          },
+          {
+            cookie_value: "http://www.example.com/?utm_campaign=spring&utm_m" +
+                          "edium=email&utm_source=newsletter1",
+            utm_campaign: "spring",
+            utm_medium: "email",
+            utm_source: "newsletter1"
+          },
+          {
+            cookie_value: "http://www.example.com/?utm_campaign=spring&utm_m" +
+                          "edium=email&utm_source=newsletter1&utm_content=to" +
+                          "plink",
+            utm_campaign: "spring",
+            utm_medium: "email",
+            utm_source: "newsletter1",
+            utm_content: "toplink"
+          },
+          {
+            cookie_value: "http://shipit.resultadosdigitais.com.br/?utm_sour" +
+                          "ce=Self+Test+List&utm_campaign=c2994af7da-xunda_m" +
+                          "other_campaign&utm_medium=email&utm_term=0_f85d50" +
+                          "388c-c2994af7da-69634449",
+            utm_source: "Self+Test+List",
+            utm_campaign: "c2994af7da-xunda_mother_campaign",
+            utm_medium: "email",
+            utm_term: "0_f85d50388c-c2994af7da-69634449"
+
+          }
+        ]
       end
+
+      let(:klass) { TrafficSourceParser::Parsers::CampaignParser }
+
+      it_behaves_like "returns the correct object with attributes"
 
     end
   end
-
 end
