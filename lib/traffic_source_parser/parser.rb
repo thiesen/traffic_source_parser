e62cd1be73976ad49793d4b6fbb51c3764dd20e4
thiesen <nathan.thiesen@outlook.com>
2015-07-24 15:32:54 -0300

    improve campaign regex match

--- lib/traffic_source_parser/parser.rb
+++ lib/traffic_source_parser/parser.rb
@@ -18,7 +18,8 @@ module TrafficSourceParser
     end
 
     def is_campaign?
-      @cookie_value.include?("utm_campaign=") || @cookie_value.include?("utmcsr=")
+      @cookie_value =~ /utmccn|utmcmd|utmctr|utmcct|utm_source|utm_medium|utm_term|utm_content|
+tm_campaign/
     end
 
   end
