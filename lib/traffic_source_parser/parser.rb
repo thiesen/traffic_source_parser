7cd4bd6308ede735d340ed958d3afc696127a081
thiesen <nathan.thiesen@outlook.com>
2015-09-09 16:28:14 -0300

    create utmz specific parser

--- lib/traffic_source_parser/parser.rb
+++ lib/traffic_source_parser/parser.rb
@@ -1,5 +1,6 @@
 require "traffic_source_parser/parser/referrer_parser"
 require "traffic_source_parser/parser/campaign_parser"
+require "traffic_source_parser/parser/utmz_parser"
 
 module TrafficSourceParser
   module Parser
@@ -14,12 +15,16 @@ module TrafficSourceParser
 
     def parser
       return CampaignParser if is_campaign?
+      return UtmzParser if is_utmz?
       ReferrerParser
     end
 
     def is_campaign?
-      @cookie_value =~ /utmccn|utmcmd|utmcsr|utmctr|utmcct|utm_source|utm_medium|utm_term|utm_content|
-tm_campaign/
+      @cookie_value =~ /utm_source|utm_medium|utm_term|utm_content|utm_campaign/
+    end
+
+    def is_utmz?
+      @cookie_value =~ /utmccn|utmcmd|utmcsr|utmctr|utmcct/
     end
 
   end
--- lib/traffic_source_parser/parser/utmz_parser.rb
+++ lib/traffic_source_parser/parser/utmz_parser.rb
@@ -1,19 +1,34 @@
-require 'traffic_source_parser/parser/utmz_parser/utmz_tools'
+require 'traffic_source_parser/parser/tools'
 require 'traffic_source_parser/result/utmz'
 
 module TrafficSourceParser
   module Parser
     module UtmzParser
       extend self
-
-      extend UtmzTools
+      extend Tools
 
       def parse(utmz)
         @utmz = utmz
         TrafficSourceParser::Result::Utmz.new(utmz_hash)
       end
 
+      def utmz_params
+        get_params(@utmz)
+      end
+
+      def parsed_utmz_params
+        parse_params(utmz_params).to_h
+      end
+
+      def utmz_hash
+        translate_keys(parsed_utmz_params, utmz_params_mapper)
+      end
+
+      def utmz_params_mapper
+        @campaign_params ||= load_config('utmz_params.yml')
+      end
+
+
     end
   end
 end
-
