a746c9a169fedb9d978121c648bee96d28d6d2e3
thiesen <nathan.thiesen@outlook.com>
2015-09-09 16:26:06 -0300

    create parser tools to extract some methods

---
+++ lib/traffic_source_parser/parser/tools.rb
@@ -0,0 +1,30 @@
+require 'uri'
+
+module TrafficSourceParser
+  module Parser
+    module Tools
+      def get_params(query)
+        query.scan(/(\w+=[^&|\|]*)/).flatten
+      end
+
+      def parse_params(params_array)
+        params_array.map { |param| param.split('=') }.
+        delete_if { |params| params.size != 2 }
+      end
+
+      def translate_keys(hash, mapper)
+        translated_hash = {}
+        hash.each do |key, value|
+          translated_key = mapper[key] || key
+          translated_hash[translated_key] = URI.unescape(value)
+        end
+        translated_hash
+      end
+
+      def load_config(file)
+        YAML::load_file(File.join(TrafficSourceParser.config_path, file))
+      end
+
+    end
+  end
+end
