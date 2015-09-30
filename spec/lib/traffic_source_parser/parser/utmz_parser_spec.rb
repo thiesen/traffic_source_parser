ac305491e18841c3091ac98d3fe97b6db751b955
Thiago von Sydow <thiagovs05@gmail.com>
2015-09-30 15:28:21 -0300

    fix source name for utmz result

--- spec/lib/traffic_source_parser/parser/utmz_parser_spec.rb
+++ spec/lib/traffic_source_parser/parser/utmz_parser_spec.rb
@@ -25,7 +25,7 @@ describe TrafficSourceParser::Parser::UtmzParser do
           let(:campaign) { '(referral)' }
           let(:medium) { 'referral' }
           let(:channel) { 'Social' }
-          let(:source) { 't.co' }
+          let(:source) { 'Twitter' }
           let(:content) { '/EFzCFawFrk' }
 
           it_behaves_like 'a traffic source campaign parser with content'
@@ -36,7 +36,7 @@ describe TrafficSourceParser::Parser::UtmzParser do
           let(:campaign) { 'experimentar-ferramenta' }
           let(:medium) { 'sponsored-post' }
           let(:channel) { 'Social' }
-          let(:source) { 'facebook' }
+          let(:source) { 'Facebook' }
           let(:content) { 'organize-seus-negocios' }
 
           it_behaves_like 'a traffic source campaign parser with content'
@@ -47,49 +47,12 @@ describe TrafficSourceParser::Parser::UtmzParser do
           let(:campaign) { '20150528-ef-aprovacaoharvard' }
           let(:medium) { 'socialmedia-fe' }
           let(:channel) { 'Social' }
-          let(:source) { 'facebook.com' }
+          let(:source) { 'Facebook' }
 
           it_behaves_like 'a traffic source campaign parser'
         end
 
       end
-
-      # context 'when value is from referral' do
-      #   {
-      #     cookie_value: "210677130.1432831711.1.1.utmcsr=rakuten|utmc" +
-      #                   "cn=linkshare|utmcmd=(not set)",
-      #     source: "rakuten",
-      #     campaign: "linkshare",
-      #     channel: "Referral",
-      #     medium: "(not set)"
-      #   }
-      # end
-      #
-      # context 'when value is from other advertising' do
-      #   {
-      #     cookie_value: "10083233.1432828147.6.5.utmcsr=newsletter|ut" +
-      #                   "mccn=20150527|utmcmd=newsletter-ef|utmctr=communi" +
-      #                   "tycolleges|utmcct=programa",
-      #     source: "newsletter",
-      #     campaign: "20150527",
-      #     channel: "Other",
-      #     medium: "newsletter-ef",
-      #     term: "communitycolleges",
-      #     content: "programa"
-      #   }
-      # end
-      #
-      # context 'when value is from CPC campaign' do
-      #   {
-      #     cookie_value: "63514687.1432831892.1.1.utmcsr=adwords_gereb" +
-      #                   "oletos1min5_22072014|utmgclid=CNjQrtjy5MUCFQYXHwo" +
-      #                   "d7k0A8g|utmccn=(not set)|utmcmd=(not set)",
-      #     source: "adwords_gereboletos1min5_22072014",
-      #     campaign: "(not set)",
-      #     channel: "Paid Search",
-      #     medium: "cpc"
-      #   }
-      # end
 
     end
 
