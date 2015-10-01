7657c09cc2cfb7d7e4d55410099284f8551d95ac
thiesen <nathan.thiesen@outlook.com>
2015-10-01 17:45:52 -0300

    first version of paid search detection

--- lib/traffic_source_parser/result/search.rb
+++ lib/traffic_source_parser/result/search.rb
@@ -1,12 +1,27 @@
 require 'traffic_source_parser/result/generic'
+require 'traffic_source_parser/channels/types/paid'
 
 module TrafficSourceParser
   module Result
 
     class Search < TrafficSourceParser::Result::Generic
 
-      def initialize(search_hash)
+    PAID_TERM = {
+      "Yahoo" => /search\?p=/,
+      "Google" => /aclk\?/,
+    }
+
+      def initialize(search_hash, referrer)
         super(search_hash)
+
+        if is_paid?(search_hash, referrer)
+          self[:channel] = TrafficSourceParser::Channels::Types::Paid.channel_name
+          self[:medium] = "cpc"
+        end
+      end
+
+      def is_paid?(search_hash, referrer)
+        PAID_TERM[search_hash[:source]] =~ referrer
       end
 
     end
