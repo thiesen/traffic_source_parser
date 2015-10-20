9650a06aafefd7e2adb7e4d36877ecc5a9f311a2
Danilo Delponte <danilo.delponte@resultadosdigitais.com.br>
2015-10-20 18:28:35 -0200

    refactor referrers list and known social method

--- lib/traffic_source_parser/channels/types/social.rb
+++ lib/traffic_source_parser/channels/types/social.rb
@@ -14,13 +14,13 @@ module TrafficSourceParser
 
           # Traffic from any of approximately 400 social networks (that are not tagged as ads).
           def match_source?(traffic_source)
-            traffic_source[:medium] =~ SOCIAL_REGEX || known_social?(traffic_source[:source])
+            traffic_source[:medium] =~ SOCIAL_REGEX || get_known_social(traffic_source[:source])
           end
 
-          def known_social?(source)
-            return false unless source
+          def get_known_social(source)
+            return unless source
             known_social_sources.each{ |k, v| return k if source.downcase.match(v * '|') }
-            false
+            nil
           end
 
           def known_social_sources
